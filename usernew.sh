17c5370bcab8a9b917eb74604b99611166fa4f79
apih46 <khafiz532@gmail.com>
2022-04-12 06:50:20 +0800

    Add files via upload

---
+++ usernew.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+MYIP=$(wget -qO- ipinfo.io/ip);
+IZIN=$( curl -sS https://raw.githubusercontent.com/apih46/ip/main/list | awk '{print $4}' | grep $MYIP )
+if [ $MYIP = $IZIN ]; then
+clear
+echo -e "${green}Tahniah! Anda Dibenarkan menggunakan LukaVPN...${NC}"
+else
+clear
+echo ""
+rm -f setup.sh
+echo '                            ...                           '
+echo '        Anda Tidak Dibenarkan Menggunakan Script ini!     '
+echo '                            ...                           '
+echo '        SILA REGISTER IP ANDA DI BOT @LukaVpn_bot!     '
+echo '                        t.me/coxoox                      '
+sleep 20
+exit 0
+fi
+clear
+
+read -p "Username : " Login
+read -p "Password : " Pass
+read -p "Expired (hari): " masaaktif
+IP=$(wget -qO- icanhazip.com);
+domain=$(cat /etc/v2ray/domain);
+IP=$(wget -qO- ipinfo.io/ip);
+ssl="$(cat ~/log-install.txt | grep -w "Stunnel4" | cut -d: -f2)"
+sqd="$(cat ~/log-install.txt | grep -w "Squid" | cut -d: -f2)"
+ovpn="$(netstat -nlpt | grep -i openvpn | grep -i 0.0.0.0 | awk '{print $4}' | cut -d: -f2)"
+ovpn2="$(netstat -nlpu | grep -i openvpn | grep -i 0.0.0.0 | awk '{print $4}' | cut -d: -f2)"
+sleep 1
+echo Ping Host
+echo Cek Hak Akses...
+sleep 0.5
+echo Permission Accepted
+clear
+sleep 0.5
+echo Membuat Akun: $Login
+sleep 0.5
+echo Setting Password: $Pass
+sleep 0.5
+clear
+useradd -e `date -d "$masaaktif days" +"%Y-%m-%d"` -s /bin/false -M $Login
+exp="$(chage -l $Login | grep "Account expires" | awk -F": " '{print $2}')"
+echo -e "$Pass\n$Pass\n"|passwd $Login &> /dev/null
+echo -e ""
+echo -e "Enjoy High Speed Internet"
+echo -e "         LukaVPN        "
+echo -e "OpenVPN Premium Account Info"
+echo -e ""
+echo -e "==============================="
+echo -e "Username       : $Login "
+echo -e "Password       : $Pass"
+echo -e "Expired On     : $exp"
+echo -e "==============================="
+echo -e ""
+echo -e "     Script Mod by LukaVPN"
